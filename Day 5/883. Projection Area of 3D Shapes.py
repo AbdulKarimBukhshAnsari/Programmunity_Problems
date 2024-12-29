99f1ea1324a6b359c097640cb9cbb22d662abcda
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-12-29 07:51:27 +0500

    Solved oms|100%

--- Day 5/883. Projection Area of 3D Shapes.py
+++ Day 5/883. Projection Area of 3D Shapes.py
@@ -1,20 +1,18 @@
-
-def projectionArea():
-    """
-    :type grid: List[List[int]]
-    :rtype: int
-    """
-    grid = [[1,2],[3,4]]
-    top = 0 
-    front = 0 
-    side = 0
-    top = sum(1 for row in grid for cell in row if cell > 0)
-    side = sum(max(grid[i]) for i in range(len(grid)))
-    for i in range(len(grid)):
-        max_element = 0 
-        for j in range(len(grid)):
-            max_element = max(grid[j][i], max_element)
-        front+= max_element
-    return top+front+side
-
-print(projectionArea())
+class Solution(object):
+    def projectionArea(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+    
+        top = 0 
+        front = 0 
+        side = 0
+        top = sum(1 for row in grid for cell in row if cell > 0)
+        side = sum(max(grid[i]) for i in range(len(grid)))
+        for i in range(len(grid)):
+            max_element = 0 
+            for j in range(len(grid)):
+                max_element = max(grid[j][i], max_element)
+            front+= max_element
+        return top+front+side
